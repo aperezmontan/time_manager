25745f7fbbf11088e9ee0f963f64d5767511e38c
Aristides Perez <aris.a.perez@gmail.com>
2023-01-14 11:39:40 -0500

    Setup with docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+echo "ENVIRONMENT: $RAILS_ENV"
+
+# install missing gems
+bundle check || bundle install --jobs 20 --retry 5
+
+# remove pid from previous session (puma server)
+
+rm -f $APP_PATH/tmp/pids/server.pid
+
+# run anything by prepending bundle exec to the passed command
+
+bundle exec ${@}
