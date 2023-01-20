110698cef5ced9ff51c0a9dab58b82b4fcb41bae
Aristides Perez <aris.a.perez@gmail.com>
2023-01-20 06:14:50 -0500

    Validations!

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,13 @@
 class Project < ApplicationRecord
   # ASSOCIATIONS
   has_many :project_updates, dependent: :destroy, autosave: true
+  # ATTEMPT TO BE MORE EFFICIENT
+  # has_one :latest_update, lambda {
+  #                           ProjectUpdate.latest_updates_for_projects
+  #                         }, class_name: 'ProjectUpdate', inverse_of: :project, dependent: :destroy
+
+  # NESTED ATTRIBUTES
+  accepts_nested_attributes_for :project_updates
 
   # VALIDATIONS
   validates :name, presence: true
--- app/models/project_update.rb
+++ app/models/project_update.rb
@@ -6,6 +6,15 @@ class ProjectUpdate < ApplicationRecord
 
   # Have to do it like this because for some reason I get an undefined method error
   # if I try to put it in a helper or concern 🤷‍♂️
-  enum time_status: { start: 'start', pause: 'pause', hold: 'hold', finish: 'finish' }
+  enum stop_status: { start: 'start', pause: 'pause', hold: 'hold', finish: 'finish' }
   enum reason: { blocked_by_SME: 'blocked_by_SME', other: 'other' }
+
+  # VALIDATIONS
+  validates :stop_status, presence: { unless: :is_start }
+
+  # ATTEMPT TO BE MORE EFFICIENT
+  # def self.latest_updates_for_projects
+  #   latest_updates_ids = select(:id).order(manually_edited_time: :desc).group(:project_id).limit(1)
+  #   where(id: latest_updates_ids)
+  # end
 end
