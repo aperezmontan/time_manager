310896a61c694fbc293ea6df95df03c1c0c08a34
Aristides Perez <aris.a.perez@gmail.com>
2023-01-20 06:14:24 -0500

    Change time_status to stop_status

--- db/migrate/20230119210104_update_fields_on_project_update.rb
+++ db/migrate/20230119210104_update_fields_on_project_update.rb
@@ -1,6 +1,6 @@
 class UpdateFieldsOnProjectUpdate < ActiveRecord::Migration[7.0]
   def change
-    rename_column :project_updates, :reason, :stop_reason
     add_column :project_updates, :is_start, :boolean, default: true, null: false
+    add_column :project_updates, :manually_edited_time, :datetime, default: -> { 'CURRENT_TIMESTAMP' }
   end
 end
